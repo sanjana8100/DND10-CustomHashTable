9806deb335eacd295148bc6c30af521421c0ba16
sanjana8100 <sanjanams812@gmail.com>
2023-06-25 14:12:51 +0530

    added: M2_Add

--- CustomHashTable.cs
+++ CustomHashTable.cs
@@ -15,6 +15,40 @@ namespace DND10_CustomHashTable
             Names = new Node[size];
         }
 
+        public int CustomHashFunction(string key)
+        {
+            char[] character = key.ToCharArray();
+            int sumofASCICode = 0;
+
+            foreach (char c in character)
+            {
+                sumofASCICode += c;
+            }
+            return sumofASCICode;
+        }
+
+        public void Add(string newName)
+        {
+            int arrayIndex = Math.Abs(CustomHashFunction(newName)) % Names.Length;
+            Node newNode = new Node(newName);
+
+            if (Names[arrayIndex] == null)
+            {
+                Names[arrayIndex] = newNode;
+            }
+            else
+            {
+                Node temp = Names[arrayIndex];
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = newNode;
+            }
+
+            Console.WriteLine($"{newName} added at index {arrayIndex}");
+        }
+
         public void Display()
         {
             for (int i = 0; i < Names.Length; i++)
--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             CustomHashTable customHashTable = new CustomHashTable(7);
+
+            customHashTable.Add("Apple");
+            customHashTable.Add("Orange");
+            customHashTable.Add("Kiwi");
+            customHashTable.Add("Mango");
+
             customHashTable.Display();
         }
     }
